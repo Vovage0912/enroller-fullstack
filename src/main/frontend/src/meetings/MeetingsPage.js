b9179b47270b276352dbeadb728a907ae6f302e7
Student <student@d17-428-pc-04.ipa.ii.agh.edu.pl>
2024-05-19 12:06:53 +0200

    Usuwanie zajec

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import NewMeetingForm from "./NewMeetingForm";
 import MeetingsList from "./MeetingsList";
 
@@ -6,15 +6,43 @@ export default function MeetingsPage({username}) {
     const [meetings, setMeetings] = useState([]);
     const [addingNewMeeting, setAddingNewMeeting] = useState(false);
 
-    function handleNewMeeting(meeting) {
-        const nextMeetings = [...meetings, meeting];
-        setMeetings(nextMeetings);
-        setAddingNewMeeting(false);
+    async function handleNewMeeting(meeting) {
+
+        const response = await fetch('/api/meetings', {
+
+            method: 'POST',
+
+            body: JSON.stringify(meeting),
+
+            headers: { 'Content-Type': 'application/json' }
+
+        });
+
+        if (response.ok) {
+
+            const addedMeeting = await response.json();
+
+            const nextMeetings = [...meetings, addedMeeting];
+
+            setMeetings(nextMeetings);
+
+            setAddingNewMeeting(false);
+
+        }
+
     }
 
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
+
+
+    async function handleDeleteMeeting(meeting) {
+
+        const response = await fetch(`/api/meetings/${meeting.id}`, {
+            method: 'DELETE',
+        }); if (response.ok) {
+            const nextMeetings = meetings.filter(m => m !== meeting);
+            setMeetings(nextMeetings);
+        }
+
     }
 
     function handleSignIn(meeting) {
@@ -36,6 +64,17 @@ export default function MeetingsPage({username}) {
         });
         setMeetings(nextMeetings);
     }
+
+    useEffect(() => {
+        const fetchMeetings = async () => {
+            const response = await fetch(`/api/meetings`);
+            if (response.ok) {
+                const meetings = await response.json();
+                setMeetings(meetings);
+            }
+        };
+        fetchMeetings();
+    }, []);
 
     return (
         <div>
